86eb5187d554dd8a1a60f9ae78812f102497c64e
Michael Yang <yql.michael@gmail.com>
2024-06-29 11:38:09 +0800

    fix path and update readme

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ Tech Stack: React, NodeJS, SCSS, GSAP, Lottie
 
 Build Tool: Vite, Webpack
 
-Deployment Tool: Netlify
-
-You can find the website at https://myql.me 
+[Link to website](https://michaelyql.github.io/portfolio/)
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,15 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'portfolio/',
   build: {
-    outDir: path.join(__dirname, "docs")
-  }
+    outDir: path.join(__dirname, "docs"),
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
 })
